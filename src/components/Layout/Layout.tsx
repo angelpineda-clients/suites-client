206a24eaf599779ff8fdb1b2fef86bf5d89b94e6
angelpineda-dev <angeljpa95@gmail.com>
2024-10-29 10:41:18 -0600

    update layout with toolpad

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,94 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import { useUserStore } from "@/store/stores";
+import { extendTheme } from "@mui/material/styles";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import DescriptionIcon from "@mui/icons-material/Description";
+import LayersIcon from "@mui/icons-material/Layers";
+import { Navigation, Router } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { useUserStore } from "@/store/user";
 import { useAuth } from "@/context/auth/AuthProvider";
 
 import { useNavigate } from "react-router";
+import React from "react";
+import { AppProvider } from "@toolpad/core/react-router-dom";
 
 interface LayoutProps {
 	children: React.ReactNode;
+}
+
+const NAVIGATION: Navigation = [
+	{
+		kind: "header",
+		title: "Main items",
+	},
+	{
+		segment: "dashboard",
+		title: "Dashboard",
+		icon: <DashboardIcon />,
+	},
+	{
+		segment: "orders",
+		title: "Orders",
+		icon: <ShoppingCartIcon />,
+	},
+	{
+		kind: "divider",
+	},
+	{
+		kind: "header",
+		title: "Analytics",
+	},
+	{
+		segment: "reports",
+		title: "Reports",
+		icon: <BarChartIcon />,
+		children: [
+			{
+				segment: "sales",
+				title: "Sales",
+				icon: <DescriptionIcon />,
+			},
+			{
+				segment: "traffic",
+				title: "Traffic",
+				icon: <DescriptionIcon />,
+			},
+		],
+	},
+	{
+		segment: "integrations",
+		title: "Integrations",
+		icon: <LayersIcon />,
+	},
+];
+
+const demoTheme = extendTheme({
+	colorSchemes: { light: true, dark: true },
+	colorSchemeSelector: "class",
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 600,
+			lg: 1200,
+			xl: 1536,
+		},
+	},
+});
+
+function useDemoRouter(initialPath: string): Router {
+	const [pathname, setPathname] = React.useState(initialPath);
+
+	const router = React.useMemo(() => {
+		return {
+			pathname,
+			searchParams: new URLSearchParams(),
+			navigate: (path: string | URL) => setPathname(String(path)),
+		};
+	}, [pathname]);
+
+	return router;
 }
 
 const Layout = ({ children }: LayoutProps) => {
@@ -13,40 +96,12 @@ const Layout = ({ children }: LayoutProps) => {
 	const auth = useAuth();
 	const navigate = useNavigate();
 
+	const router = useDemoRouter("/dashboard");
+
 	return (
-		<>
-			<AppBar position="static" sx={{ backgroundColor: "white" }}>
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="primary"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-						onClick={() => navigate("/")}
-					>
-						logo
-					</IconButton>
-					<Typography
-						variant="subtitle2"
-						component="div"
-						sx={{ flexGrow: 1, mr: 2 }}
-						color="primary"
-						textAlign="right"
-					>
-						user name
-					</Typography>
-					<Button
-						color="primary"
-						onClick={auth.logout}
-						sx={{ border: "thin solid" }}
-					>
-						Logout
-					</Button>
-				</Toolbar>
-			</AppBar>
-			{children}
-		</>
+		<AppProvider navigation={NAVIGATION} router={router} theme={demoTheme}>
+			<DashboardLayout>{children}</DashboardLayout>
+		</AppProvider>
 	);
 };
 
