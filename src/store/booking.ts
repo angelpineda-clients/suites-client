7efb666ad7dda7fe748a5484001d79c846794e4f
angelpineda-dev <angeljpa95@gmail.com>
2024-12-18 11:50:03 -0600

    add booking store

---
+++ src/store/booking.ts
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export interface IStoreNewBooking {
+	check_in: string | null;
+	check_out: string | null;
+	adults: string;
+	children: string;
+}
+
+export interface IStoreBooking {
+	checkIn: string | null;
+	checkOut: string | null;
+	adults: string;
+	children: string;
+}
+
+interface IBookingStore {
+	booking: IStoreBooking;
+	setBooking: (data: IStoreNewBooking) => void;
+}
+
+export const useBookingStore = create<IBookingStore>()(
+	devtools((set) => ({
+		booking: {
+			checkIn: null,
+			checkOut: null,
+			adults: "2",
+			children: "0",
+		},
+		setBooking: (data: IStoreNewBooking) => {
+			const { check_in, check_out, adults, children } = data;
+
+			set({
+				booking: {
+					checkIn: check_in,
+					checkOut: check_out,
+					adults,
+					children,
+				},
+			});
+		},
+	}))
+);
